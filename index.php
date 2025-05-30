177e64419ede02987bbcab5c5f5c43dc6d0b49e3
8ctopus <hello@octopuslabs.io>
2025-05-30 10:44:05 +0400

    Update composer dependencies

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use Apix\Log\Format\Minimal;
+use Apix\Log\Format\MinimalColors;
 use Apix\Log\Logger;
 use Apix\Log\Logger\Stream;
 use NunoMaduro\Collision\Provider as ExceptionHandler;
@@ -18,7 +18,7 @@ $stdout = (new Stream('php://stdout'))
     ->setMinLevel('debug')
     // propagate to other loggers
     ->setCascading(true)
-    ->setFormat(new Minimal());
+    ->setFormat(new MinimalColors());
 
 $logger = new Logger([$stdout]);
 
