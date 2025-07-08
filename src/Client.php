c2491378f1961b834b2c12a86959ec015216c1a4
8ctopus <hello@octopuslabs.io>
2025-07-08 11:19:35 +0400

    Fix wordpress redirects query keys with equal sign without value

--- src/Client.php
+++ src/Client.php
@@ -38,7 +38,11 @@ class Client
         $key = array_key_first($this->cacheBusting);
         $query->set($key, $this->cacheBusting[$key]);
 
-        return (new Request('GET', $url->toString()))
+        $url = $url->toString();
+
+        $url = str_replace('nocache=', 'nocache', $url);
+
+        return (new Request('GET', $url))
             ->withHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36')
             ->withHeader('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8')
             ->withHeader('Accept-Language', 'en-US,en;q=0.9')
